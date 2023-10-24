21416dcdac58c1369d4cd5c45ac63baadd8ba688
cmeowwww <a3508178@gmail.com>
2023-10-24 11:47:27 +0800

    week07

---
+++ week07/week07-5.py
@@ -0,0 +1,5 @@
+s = "abcdabcd" #字串
+d = {} #字典
+for c in s:
+  d[c] = 1
+  print('現在讀到的字母' , c, "字典變成", d)
---
+++ week07/week07-6.py
@@ -0,0 +1,8 @@
+s = "abcdabcd" #字串
+d = {} #字典
+for c in s:
+  if c in d:
+    d[c] = d[c] + 1
+  else:
+    d[c] = 1
+  print('現在讀到的字母' , c, "字典變成", d)
