ef205fd9f60e78b62fd5d58558077f55045653cc
cmeowwww <a3508178@gmail.com>
2023-12-12 11:56:14 +0800

    week14

---
+++ week14/week14-3b.py
@@ -0,0 +1,4 @@
+#瘋狂程設 　|　SOIT106_ADVANCE_004 進階題：大小寫轉換
+
+a =input()
+print(a.swapcase())
---
+++ week14/week14-4a.py
@@ -0,0 +1,9 @@
+#瘋狂程設 　|　SOIT106_ADVANCE_007 進階題：迴文判斷
+
+
+a =input()
+
+if a[0]==a[3]and a[1]==a[2]:
+	print('YES')
+else:
+	print('NO')
