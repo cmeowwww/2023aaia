f1c7d5d1afb5ce328b0d432d2d0b8bbe1bb7427a
cmeowwww <a3508178@gmail.com>
2024-01-09 11:53:16 +0800

    week18

---
+++ week18/week18-1a.py
@@ -0,0 +1,6 @@
+#瘋狂程設 　|SOIT106_BASE_003 基礎題：N數之和
+
+
+a=list(map(int,input().split()))
+
+print(sum(a)-a[0])
---
+++ week18/week18-2c.py
@@ -0,0 +1,12 @@
+
+#瘋狂程設 　|SOIT106_BASE_011 基礎題：判斷大小
+
+a,b=list(map(int,input().split()))
+
+if a<b: ans=-1
+
+if a>b: ans=1
+
+if a==b: ans=0
+
+print(ans)
